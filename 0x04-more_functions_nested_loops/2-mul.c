7093309fe99a7df64207ade3d38c366f02906be3
DocMunyati <paddymunyati@gmail.com>
2022-09-16 06:01:52 -0700

    2mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,24 +2,16 @@
 
 
 
-/**
+/*
 
 *mul - multiplies two numbers
-
 *@a: first number
-
 *@b: second number
-
 *Return: returns result
-
 */
 
 
-
 int mul(int a, int b)
-
 {
-
 	return (a * b);
-
 }
