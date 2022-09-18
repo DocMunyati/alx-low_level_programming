ed7f3addf1270b6156e31bf2a61ac3c39fe44751
DocMunyati <paddymunyati@gmail.com>
2022-09-18 12:49:11 -0700

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
-nclude <unistd.h>
+#include <unistd.h>
 
-/* Description: main - prints out the stdout
+/**
+
+ * Description: main - prints out the stdout
+
  * Return: 1 if successful
+
  */
 
 int main(void)
 
-{ 	char Str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{
+
+	char Str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+
 	write(2, Str1, 59);
+
 	return (1);
+
 }
